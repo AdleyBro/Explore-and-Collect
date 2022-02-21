b2002d4edeb43917460dfa90f070c741cbd15b00
AdleyMD <android2026@hotmail.com>
2022-02-21 14:31:24 +0100

    Starting point I guess

---
+++ src/main/java/adleymd/exploreandcollect/Exploreandcollect.java
@@ -0,0 +1,10 @@
+package adleymd.exploreandcollect;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Exploreandcollect implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
